2f35d387b29a0e766e957c1fcbb16cd8ac417e87
Jesse Dietrichson <jesse.dietrichson@xamarin.com>
2019-02-07 03:57:53 -0500

    Add project files.

---
+++ CreateGrowingArray/GrowingArray.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreateGrowingArray
+{
+    class GrowingArray
+    {
+        private int[] intArray;
+        private int currentPosition = 0;
+
+        public GrowingArray()
+        {
+            intArray = new int[2];
+        }
+
+        public void Add(int intToAdd)
+        {
+            intArray[currentPosition] = intToAdd;
+            currentPosition++;
+
+            if (currentPosition >= intArray.Length)
+            {
+                //grow array
+                int[] tempArray = new int[intArray.Length * 2];
+                //copy the data to the new array
+                for (int i = 0; i < intArray.Length; i++)
+                {
+                    tempArray[i] = intArray[i];
+                }
+
+                intArray = tempArray;
+            }
+        }
+        public void Display()
+        {
+            foreach (int x in intArray)
+            {
+                if (x != 0)
+                {
+                    Console.WriteLine(x);
+                }
+            }
+        }
+    }
+}
---
+++ CreateGrowingArray/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreateGrowingArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GrowingArray array = new GrowingArray();
+
+            array.Add(5);
+            array.Add(60);
+            array.Add(4);
+            array.Add(56);
+            array.Add(12);
+            array.Add(45);
+            array.Add(45);
+            array.Add(45);
+
+            array.Display();
+        }
+    }
+}
